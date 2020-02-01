af0cefc5013e24b7582ff893db3d4a3bffe9a581
Dev <devparmar967@gmail.com>
2020-02-01 21:12:51 +0530

    for mobile platform

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
 new fullpage('#main',{
-    navigation: true,
     navigationTooltips: ['HOME','SKILLS','CONTACT','ABOUT','PROJECTS'],
     loopBottom: true,
-    showActiveTooltip: true,
     controlArrows: false,
     slidesNavigation:true,
     anchors:['HOME','SKILLS','CONTACT','ABOUT','PROJECTS'],
